1038863824b5cc55fc7953e79009422dc4e4355c
YAHIRKRP <114432531+YAHIRRoger@users.noreply.github.com>
2022-12-06 23:38:44 -0600

    Add files via upload

---
+++ Piedra_Papel_Tijera/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piedra_Papel_Tijera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Piedra, Papel o Tijera");
+            while (true)
+            {
+                Console.WriteLine("Coloca la opcion con la que quieras comenzar: ");
+                string eleccion = Console.ReadLine().ToUpper();
+                if (eleccion == "SALIR")
+                {
+                    break;
+                }
+                if (eleccion == "PIEDRA" || eleccion == "PAPEL" || eleccion == "TIJERA")
+                {
+              Random rnd = new Random();
+                    int numeroAleatorio = rnd.Next(0, 3);
+                    string[] opciones = new string[] { "PIEDRA", "PAPEL", "TIJERA" };
+                    string eleccionMaquina = opciones[numeroAleatorio];
+                    Console.WriteLine("La máquina escogio: " + eleccionMaquina);
+                    if (eleccion == eleccionMaquina)
+                    {
+                        Console.WriteLine("EMPATE");
+                    }
+                    else if (eleccion == "PIEDRA" && eleccionMaquina == "TIJERA")
+                    {
+                        Console.WriteLine("GANASTE");
+                    }
+                    else if (eleccion == "PAPEL" && eleccionMaquina == "PIEDRA")
+                    {
+                        Console.WriteLine("GANASTE");
+                    }
+                    else if (eleccion == "TIJERA" && eleccionMaquina == "PAPEL")
+                    {
+                        Console.WriteLine("GANASTE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("PERDISTE");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: Escribe Piedra, Papel o Tijera");
+                }
+            }
+
+
+        }
+    }
+}
